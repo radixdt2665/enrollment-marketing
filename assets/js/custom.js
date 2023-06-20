0d24bd4411eb8386f134c2ef43a9a5e537386704
Shraddha Gediya <shraddha.gediya@GALAXY.RADIXWEB.COM>
2023-06-20 13:45:42 +0530

    work in program assessment and upload data page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,15 @@
 $(document).ready(function() { 
     pageHeight();
+    sideNav();
     $(window).resize(function(){
         pageHeight();
+        sideNav();
     });
     function pageHeight() {
         $('.page-content').css("min-height", "calc(100vh - " + $('.page-footer').outerHeight() + "px)" );
+    }
+    function sideNav() {
+        $('.side-nav').css("height", "calc(100vh - " + $('.custom-nav').outerHeight() + "px)" );
     }
 
    
