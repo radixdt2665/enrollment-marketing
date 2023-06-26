0889ec4b842b17d76caa9171e08b99cd28281765
Shraddha Gediya <shraddha.gediya@GALAXY.RADIXWEB.COM>
2023-06-26 18:33:21 +0530

    bug fixes in login page and add chart img in program planner page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,98 +1,102 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $(this).scrollTop(0);
   pageHeight();
   fixSidebarOnScroll();
-  $(window).resize(function () {
-    pageHeight();
-    fixSidebarOnScroll();
+  $(window).resize(function() {
+      pageHeight();
+      fixSidebarOnScroll();
   });
+
   function pageHeight() {
-    $(".login .page-content").css(
-      "min-height",
-      "calc(100vh - " + $(".page-footer").outerHeight() + "px)"
-    );
-    if (
-      document.querySelector(".side-nav") !== null &&
-      document.querySelector(".content-wrapper-main") !== null
-    ) {
-      $(".side-nav, .content-wrapper-main").css(
-        "height",
-        "calc(100vh - " + $(".custom-nav").outerHeight() + "px)"
+      $(".login .page-content").css(
+          "min-height",
+          "calc(100vh - " + $(".page-footer").outerHeight() + "px)"
       );
-      $(".content-wrapper-main").css(
-        "width",
-        "calc(100% - " + $(".side-nav").outerWidth() + "px)"
+      if (document.querySelector(".side-nav") !== null) {
+          $(".side-nav").css(
+              "height",
+              "calc(100vh - " + $(".custom-nav").outerHeight() + "px)"
+          );
+      }
+      if (document.querySelector(".content-body") !== null) {
+          $(".content-body").css(
+              "min-height",
+              "calc(100vh - " +
+              ($(".custom-nav").outerHeight() + $(".page-footer").outerHeight()) +
+              "px)"
+          );
+      }
+      $(".side-nav").css(
+          "height",
+          "calc(100vh - " + $(".custom-nav").outerHeight() + "px)"
       );
-    }
-    if (document.querySelector(".content-body") !== null) {
-      $(".content-body").css(
-        "min-height",
-        "calc(100vh - " +
-          ($(".custom-nav").outerHeight() + $(".page-footer").outerHeight()) +
-          "px)"
-      );
-    }
-    $(".side-nav").css(
-      "height",
-      "calc(100vh - " + $(".custom-nav").outerHeight() + "px)"
-    );
-    $(".left-sec.sticky").css("min-height","calc(100vh - " + ($(".custom-nav").outerHeight() + $(".page-footer").outerHeight()) + "px)");
 
 
-    if ($(window).width() < 1199) {
-      $("body").addClass("small-side-nav");
-      $(".content-wrapper-main").css("width", "calc(100% - " + "72px)");
-    } else {
-      $("body").removeClass("small-side-nav");
-      $(".content-wrapper-main").css(
-        "width",
-        "calc(100% - " + $(".side-nav").outerWidth() + "px)"
-      );
-    }
-    
+      if ($(window).width() < 1199) {
+          $("body").addClass("small-side-nav");
+          $(".content-wrapper-main").css("width", "calc(100% - " + "72px)");
+      } else {
+          $("body").removeClass("small-side-nav");
+          $(".content-wrapper-main").css(
+              "width",
+              "calc(100% - " + $(".side-nav").outerWidth() + "px)"
+          );
+      }
+
   }
 
-  $(".sidebar-toogle").click(function () {
-    $("body").toggleClass("small-side-nav");
-    $(".content-wrapper-main").css("width", "calc(100% - " + "72px)");
+  $(".sidebar-toogle").click(function() {
+      $("body").toggleClass("small-side-nav");
+      $(".content-wrapper-main").css("width", "calc(100% - " + "72px)");
+      setTimeout(() => {
+        if (document.querySelector(".left-sec") !== null) {
+          if ($(".left-sec").hasClass("sticky")) {
+            $(".left-sec").css({"bottom": ($(".page-footer").outerHeight() + 36) + "px" , "width" : ($('.left-sec-parent').innerWidth() - 28 )+ "px", 
+                                "left" : ($(".side-nav").outerWidth() + 28) + "px"});
+          }
+        } 
+      },150);
+     
   });
 
-  $(".leftSecLinks").click(function (e) {
-    setTimeout(() => {
-      var hash = window.location.hash;
-      console.log("adawdawd", hash);
-      $(".content-wrapper-main").animate(
-        {
-          scrollTop: $(hash).offset().top - $(".custom-nav").outerHeight(),
-        },
-        500
-      );
-    },50);
+  $(".leftSecLinks").click(function(e) {
+      e.preventDefault();
+
+      var href = $(this).attr("href");
+      var hash = href.substr(href.indexOf("#"));
+      $('html, body').animate({
+          scrollTop: $(hash).offset().top - $(".custom-nav").outerHeight()
+      }, 40);
+
   });
 
-  var $sidebar = $(".left-sec");
-
-  var sidebarTop = $sidebar.position().top;
-
-  var rightSec = $(".right-sec").outerHeight() - 10;
-
-  $(".content-wrapper-main").scroll(function () {
-    fixSidebarOnScroll();
+  $(window).scroll(function() {
+      fixSidebarOnScroll();
+      $(".side-nav").css({
+          "height ": "calc(100vh - " + $(".custom-nav").outerHeight() + "px)",
+          "top": +$(".custom-nav").outerHeight() + "px"
+      });
   });
 
 
   function fixSidebarOnScroll() {
-    var windowScrollTop = $(".content-wrapper-main").scrollTop();
-    if (windowScrollTop >= rightSec || windowScrollTop <= sidebarTop) {
-      // Remove when the scroll is greater than our #content.OuterHeight()
-      // or when our sticky scroll is above the original position of the sidebar
-      $sidebar.removeClass("sticky");
-    } else if (windowScrollTop >= sidebarTop) {
-      // Otherwise add the sticky if $sidebar doesnt have it already!
-      if (!$sidebar.hasClass("sticky")) {
-        $sidebar.addClass("sticky");
-      }
-    }
-  }
+      if (document.querySelector(".left-sec") !== null) {
+          var $sidebar = $(".left-sec");
+          var sidebarTop = $sidebar.position().top;
+          var rightSec = $(".right-sec").outerHeight() - 10;
+          var windowScrollTop = $(window).scrollTop();
+          if (windowScrollTop >= rightSec || windowScrollTop <= sidebarTop) {
+              $sidebar.removeClass("sticky");
+              $(".left-sec").css({"bottom": ($(".page-footer").outerHeight() + 36) + "px" , "width" : "auto"});
 
-  
+          } else if (windowScrollTop >= sidebarTop) {
+            setTimeout(() => {
+              $(".left-sec").css({"bottom": ($(".page-footer").outerHeight() + 36) + "px" , "width" : ($('.left-sec-parent').innerWidth() - 28 )+ "px", "left": ($(".side-nav").outerWidth() + 28)});
+            },50);
+              if (!$sidebar.hasClass("sticky")) {
+                $sidebar.addClass("sticky");
+              }
+          }
+      }
+  }
 });
