3a0492ab7bbe7b8395aeae53ab25fce60faff9a7
Shraddha Gediya <shraddha.gediya@GALAXY.RADIXWEB.COM>
2023-06-27 14:51:54 +0530

    work in user guide page and change js regard user guide page.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $(this).scrollTop(0);
   pageHeight();
-  fixSidebarOnScroll();
+//   fixSidebarOnScroll();
   $(window).resize(function() {
       pageHeight();
-      fixSidebarOnScroll();
+    //   fixSidebarOnScroll();
   });
 
   function pageHeight() {
@@ -32,22 +32,32 @@ $(document).ready(function() {
       );
 
 
-      if ($(window).width() < 1199) {
-          $("body").addClass("small-side-nav");
-          $(".content-wrapper-main").css("width", "calc(100% - " + "72px)");
-      } else {
-          $("body").removeClass("small-side-nav");
-          $(".content-wrapper-main").css(
-              "width",
-              "calc(100% - " + $(".side-nav").outerWidth() + "px)"
-          );
-      }
-
-  }
-
+    }
+    
+    if ($(window).width() < 1199) {
+        $("body").addClass("small-side-nav");
+        $(".content-wrapper-main").css("width", "calc(100% - " + "72px)");
+    } else {
+        $("body").removeClass("small-side-nav");
+      //   $(".content-wrapper-main").css(
+      //       "width",
+      //       "calc(100% - " + $(".side-nav").outerWidth() + "px)"
+      //   );
+        $(".content-wrapper-main").css("width", "calc(100% - " + "260px)");
+    }
+    setTimeout(() => {
+        $(".userPageHeading").css("top", $(".custom-nav").outerHeight())
+        $(".left-sec").css("top", $(".custom-nav").outerHeight() + $(".userPageHeading").outerHeight() + 4)
+    }, 220);
   $(".sidebar-toogle").click(function() {
       $("body").toggleClass("small-side-nav");
-      $(".content-wrapper-main").css("width", "calc(100% - " + "72px)");
+      if($("body").hasClass("small-side-nav")){
+        console.log("adaw");
+          $(".content-wrapper-main").css("width", "calc(100% - " + "72px)");
+        }else{
+        console.log("dwrf");
+            $(".content-wrapper-main").css("width", "calc(100% - " + "260px)");
+      }
       setTimeout(() => {
         if (document.querySelector(".left-sec") !== null) {
           if ($(".left-sec").hasClass("sticky")) {
@@ -55,8 +65,9 @@ $(document).ready(function() {
                                 "left" : ($(".side-nav").outerWidth() + 28) + "px"});
           }
         } 
+        $(".userPageHeading").css("top", $(".custom-nav").outerHeight())
+        $(".left-sec").css("top", $(".custom-nav").outerHeight() + $(".userPageHeading").outerHeight() + 4)
       },220);
-     
   });
 
   $(".leftSecLinks").click(function(e) {
@@ -65,38 +76,37 @@ $(document).ready(function() {
       var href = $(this).attr("href");
       var hash = href.substr(href.indexOf("#"));
       $('html, body').animate({
-          scrollTop: $(hash).offset().top - $(".custom-nav").outerHeight()
+          scrollTop: $(hash).offset().top - $(".custom-nav").outerHeight() - $(".userPageHeading").outerHeight()
       }, 40);
 
   });
 
   $(window).scroll(function() {
-      fixSidebarOnScroll();
+    //   fixSidebarOnScroll();
       $(".side-nav").css({
           "height ": "calc(100vh - " + $(".custom-nav").outerHeight() + "px)",
           "top": +$(".custom-nav").outerHeight() + "px"
       });
   });
+ 
+//   function fixSidebarOnScroll() {
+//       if (document.querySelector(".left-sec") !== null) {
+//           var $sidebar = $(".left-sec");
+//           var sidebarTop = $sidebar.position().top;
+//           var rightSec = $(".right-sec").outerHeight() - 10;
+//           var windowScrollTop = $(window).scrollTop();
+//           if (windowScrollTop >= rightSec || windowScrollTop <= sidebarTop) {
+//               $sidebar.removeClass("sticky");
+//               $(".left-sec").css({"bottom": ($(".page-footer").outerHeight() + 36) + "px" , "width" : "auto"});
 
-
-  function fixSidebarOnScroll() {
-      if (document.querySelector(".left-sec") !== null) {
-          var $sidebar = $(".left-sec");
-          var sidebarTop = $sidebar.position().top;
-          var rightSec = $(".right-sec").outerHeight() - 10;
-          var windowScrollTop = $(window).scrollTop();
-          if (windowScrollTop >= rightSec || windowScrollTop <= sidebarTop) {
-              $sidebar.removeClass("sticky");
-              $(".left-sec").css({"bottom": ($(".page-footer").outerHeight() + 36) + "px" , "width" : "auto"});
-
-          } else if (windowScrollTop >= sidebarTop) {
-            setTimeout(() => {
-              $(".left-sec").css({"bottom": ($(".page-footer").outerHeight() + 36) + "px" , "width" : ($('.left-sec-parent').innerWidth() - 28 )+ "px", "left": ($(".side-nav").outerWidth() + 28)});
-            },50);
-              if (!$sidebar.hasClass("sticky")) {
-                $sidebar.addClass("sticky");
-              }
-          }
-      }
-  }
+//           } else if (windowScrollTop >= sidebarTop) {
+//             setTimeout(() => {
+//               $(".left-sec").css({"bottom": ($(".page-footer").outerHeight() + 36) + "px" , "width" : ($('.left-sec-parent').innerWidth() - 28 )+ "px", "left": ($(".side-nav").outerWidth() + 28)});
+//             },50);
+//               if (!$sidebar.hasClass("sticky")) {
+//                 $sidebar.addClass("sticky");
+//               }
+//           }
+//       }
+//   }
 });
