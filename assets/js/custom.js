8d9664a861a115cf210a4f8515c8387077c0113e
Shraddha Gediya <shraddha.gediya@GALAXY.RADIXWEB.COM>
2023-06-29 18:42:56 +0530

    change side nav link in all pages.create my profile page add select2 link in program assessment page. and add highchart js of program planner in that page.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -93,145 +93,149 @@ $(document).ready(function () {
     );
   });
 
-  // Build the chart
-  Highcharts.chart("container1", {
-    chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false,
-      type: "pie",
-    },
-    title: {
-      text: "",
-      align: "left",
-    },
-    tooltip: {
-      pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
-    },
-    accessibility: {
-      point: {
-        valueSuffix: "%",
-      },
-    },
-    plotOptions: {
-      pie: {
-        lineWidth: 4,
-        allowPointSelect: true,
-        cursor: "pointer",
-        colors: ["#ffd100", "#b1b3b4", "#b1e4e3"],
-        // borderRadius: 1,
-        dataLabels: {
-          enabled: true,
-          format: "{point.percentage:.1f} %",
-          distance: -50,
-          filter: {
-            property: "percentage",
-            operator: ">",
-            value: 4,
-          },
-        },
-        showInLegend: true,
-      },
-    },
-    series: [
-      {
-        name: "Share",
-        colorByPoint: true,
-        data: [
-          { name: "Low Opportunity", y: 42 },
-          { name: "Moderate Opportunity", y: 40 },
-          { name: "High Opportunity", y: 18 },
-        ],
-      },
-    ],
-  });
+//   // Build the chart
+//   if (document.querySelector(".container1") !== null) {
+//     Highcharts.chart("container1", {
+//       chart: {
+//         plotBackgroundColor: null,
+//         plotBorderWidth: null,
+//         plotShadow: false,
+//         type: "pie",
+//       },
+//       title: {
+//         text: "",
+//         align: "left",
+//       },
+//       tooltip: {
+//         pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
+//       },
+//       accessibility: {
+//         point: {
+//           valueSuffix: "%",
+//         },
+//       },
+//       plotOptions: {
+//         pie: {
+//           lineWidth: 4,
+//           allowPointSelect: true,
+//           cursor: "pointer",
+//           colors: ["#ffd100", "#b1b3b4", "#b1e4e3"],
+//           // borderRadius: 1,
+//           dataLabels: {
+//             enabled: true,
+//             format: "{point.percentage:.1f} %",
+//             distance: -50,
+//             filter: {
+//               property: "percentage",
+//               operator: ">",
+//               value: 4,
+//             },
+//           },
+//           showInLegend: true,
+//         },
+//       },
+//       series: [
+//         {
+//           name: "Share",
+//           colorByPoint: true,
+//           data: [
+//             { name: "Low Opportunity", y: 42 },
+//             { name: "Moderate Opportunity", y: 40 },
+//             { name: "High Opportunity", y: 18 },
+//           ],
+//         },
+//       ],
+//     });
+//   }
 
-  // degree conferred chart
-  // Data retrieved https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature
-  Highcharts.chart("container2", {
-    chart: {
-      type: "line",
-    },
-    title: {
-      text: "",
-    },
-    subtitle: {
-      text: "",
-    },
-    legend: {
-      enabled: false,
-    },
-    // line: {
-    //   point: {
-    //     events: {
-    //       click: function (event) {
-    //         this.series.data.forEach(function (point) {
-    //           if (point.color == "#2caffe") {
-    //             point.update({ color: "#434348" });
-    //           }
-    //         });
-    //         this.update({ color: "#1a1aff" });
-    //       },
-    //     },
-    //   },
-    // },
-    xAxis: {
-      categories: [
-        "100",
-        "120",
-        "140",
-        "160",
-        "180",
-        "200",
-        "220",
-        "240",
-        "260",
-        "280",
-      ],
-    },
-    yAxis: {
-      title: {
-        text: "",
-      },
-    },
-    plotOptions: {
-      line: {
-        dataLabels: {
-          enabled: true,
-        },
-        enableMouseTracking: false,
-      },
-    },
-    series: [
+//   // degree conferred chart
+//   // Data retrieved https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature
+//   if (document.querySelector(".container2") !== null) {
+//   Highcharts.chart("container2", {
+//     chart: {
+//       type: "line",
+//     },
+//     title: {
+//       text: "",
+//     },
+//     subtitle: {
+//       text: "",
+//     },
+//     legend: {
+//       enabled: false,
+//     },
+//     // line: {
+//     //   point: {
+//     //     events: {
+//     //       click: function (event) {
+//     //         this.series.data.forEach(function (point) {
+//     //           if (point.color == "#2caffe") {
+//     //             point.update({ color: "#434348" });
+//     //           }
+//     //         });
+//     //         this.update({ color: "#1a1aff" });
+//     //       },
+//     //     },
+//     //   },
+//     // },
+//     xAxis: {
+//       categories: [
+//         "100",
+//         "120",
+//         "140",
+//         "160",
+//         "180",
+//         "200",
+//         "220",
+//         "240",
+//         "260",
+//         "280",
+//       ],
+//     },
+//     yAxis: {
+//       title: {
+//         text: "",
+//       },
+//     },
+//     plotOptions: {
+//       line: {
+//         dataLabels: {
+//           enabled: true,
+//         },
+//         enableMouseTracking: false,
+//       },
+//     },
+//     series: [
 
-        {
+//         {
   
-          name: "",
+//           name: "",
   
-          data: [
+//           data: [
   
-            {
+//             {
   
              
   
-              y: 16,
+//               y: 16,
   
-              y: 20,
+//               y: 20,
   
-              y: 30,
+//               y: 30,
   
-              y: 40,
+//               y: 40,
   
-              color: "#63666a" /* color of the unfilled part of the bar */,
+//               color: "#63666a" /* color of the unfilled part of the bar */,
   
-            },
+//             },
   
-          ],
+//           ],
   
-        },
+//         },
   
-      ],
-  });
+//       ],
+//   });
+// }
 
   $(window).scroll(function () {
     //   fixSidebarOnScroll();
